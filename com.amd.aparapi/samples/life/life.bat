c56af873c37051b29577829b48f0346af3d2ebca
Matthew Webber <matthew.webber@diamond.ac.uk>
2012-08-17 11:36:23 +0100

    ADMIN: remove samples from project

--- com.amd.aparapi/samples/life/life.bat
+++
@@ -1,8 +0,0 @@
-java ^
- -Djava.library.path=../.. ^
- -Dcom.amd.aparapi.executionMode=%1 ^
- -Dcom.amd.aparapi.enableProfiling=true ^
- -classpath ../../aparapi.jar;life.jar ^
- com.amd.aparapi.sample.life.Main
-
-
