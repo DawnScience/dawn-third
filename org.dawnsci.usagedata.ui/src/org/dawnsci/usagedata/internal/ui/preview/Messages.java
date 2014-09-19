782b91e259217db0cf9808c018f170557acb59c8
matthew.gerring <matthew.gerring@gmail.com>
2014-09-19 10:36:46 +0100

    EPL Header

--- org.dawnsci.usagedata.ui/src/org/dawnsci/usagedata/internal/ui/preview/Messages.java
+++ org.dawnsci.usagedata.ui/src/org/dawnsci/usagedata/internal/ui/preview/Messages.java
@@ -1,14 +1,11 @@
-/*******************************************************************************
-* Copyright (c) 2005 IBM Corporation and others.
-*
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*     IBM - initial API and implementation
-*******************************************************************************/
+/*
+ * Copyright (c) 2012 Diamond Light Source Ltd.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 
 package org.dawnsci.usagedata.internal.ui.preview;
 
