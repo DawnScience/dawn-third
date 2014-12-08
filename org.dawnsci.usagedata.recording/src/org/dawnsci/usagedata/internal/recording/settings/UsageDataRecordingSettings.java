f8c3e1ad39568f918843f837f7710fa60d592ca3
Baha El Kassaby <baha.el-kassaby@diamond.ac.uk>
2014-12-08 10:54:07 +0000

    Set default value of recording.ask to false: fix to http://jira.diamond.ac.uk/browse/SCI-2095

--- org.dawnsci.usagedata.recording/src/org/dawnsci/usagedata/internal/recording/settings/UsageDataRecordingSettings.java
+++ org.dawnsci.usagedata.recording/src/org/dawnsci/usagedata/internal/recording/settings/UsageDataRecordingSettings.java
@@ -53,7 +53,7 @@ public class UsageDataRecordingSettings implements UploadSettings {
 	public static final int PERIOD_REASONABLE_MINIMUM = 15 * 60 * 1000; // 15 minutes
 	static final int UPLOAD_PERIOD_DEFAULT = 5 * 24 * 60 * 60 * 1000; // five days
 	static final String UPLOAD_URL_DEFAULT = "http://diamweb01.diamond.ac.uk/dawnstats/upload.php"; //$NON-NLS-1$
-	static final boolean ASK_TO_UPLOAD_DEFAULT = true;
+	static final boolean ASK_TO_UPLOAD_DEFAULT = false;
 
 	private PreferencesBasedFilter filter = new PreferencesBasedFilter();
 
